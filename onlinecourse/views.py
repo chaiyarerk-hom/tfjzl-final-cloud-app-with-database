d82dae7f6c0bbeb7327dcbb4b6bb4d7adbc7e987
chaiyarer-hom <chaiyarerk.hom@zohomail.com>
2025-10-03 08:27:43 -0400

    complete

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -155,6 +155,6 @@ def show_exam_result(request, course_id, submission_id):
     context['grade'] = total_score
     context['choices'] = choices
 
-    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)c
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
